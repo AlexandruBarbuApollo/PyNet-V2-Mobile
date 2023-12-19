44b62191392bc75175aca33242eb99fe0d706735
AlexandruBarbuApollo <alexandru.barbu@apolloaitech.com>
2023-12-19 10:42:04 +0200

    updated files for Kaggle

--- train.sh
+++ train.sh
@@ -1,21 +1,20 @@
-python train_model_keras.py level=3 restore_iter=0 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="vgg+ssim"
-# cp models/model.3.best.h5 models/model.4.9999.h5
-# python train_model_keras.py level=3 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="mse+ssim"
-# cp models/model.3.best.h5 models/model.3.9999.h5
-# python train_model_keras.py level=2 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="vgg+ssim"
-# cp models/model.2.best.h5 models/model.3.9999.h5
-# python train_model_keras.py level=2 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="mse+ssim"
-# cp models/model.2.best.h5 models/model.2.9999.h5
-# python train_model_keras.py level=1 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="vgg+ssim"
-# cp models/model.1.best.h5 models/model.2.9999.h5
-# python train_model_keras.py level=1 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="mse+ssim"
-# cp models/model.1.best.h5 models/model.2.9999.h5
+python train_model_keras.py level=3 restore_iter=0 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="vgg+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.3.best.h5 /kaggle/working/PyNET-v2/models/model.4.9999.h5
+# python train_model_keras.py level=3 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="mse+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.3.best.h5 /kaggle/working/PyNET-v2/models/model.3.9999.h5
+# python train_model_keras.py level=2 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="vgg+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.2.best.h5 /kaggle/working/PyNET-v2/models/model.3.9999.h5
+# python train_model_keras.py level=2 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="mse+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.2.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
+# python train_model_keras.py level=1 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="vgg+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.1.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
+# python train_model_keras.py level=1 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="mse+ssim"
+# cp /kaggle/working/PyNET-v2/models/model.1.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
 
 # fine tuning
-#CUDA_VISIBLE_DEVICES=1 python train_model_keras.py level=1 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="ssim"
-#cp models/model.1.best.h5 models/model.2.9999.h5
-#python train_model_keras.py level=1 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="vgg+ssim"
-#cp models/model.1.best.h5 models/model.2.9999.h5
-#python train_model_keras.py level=1 restore_iter=9999 dataset_dir=raw_images/ train_size=95000 dir_prefix= model_path=model.py loss_fn="mse+ssim"
-#cp models/model.1.best.h5 models/model.2.9999.h5
-
+#CUDA_VISIBLE_DEVICES=1 python train_model_keras.py level=1 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="ssim"
+#cp /kaggle/working/PyNET-v2/models/model.1.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
+#python train_model_keras.py level=1 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="vgg+ssim"
+#cp /kaggle/working/PyNET-v2/models/model.1.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
+#python train_model_keras.py level=1 restore_iter=9999 dataset_dir=/kaggle/input/zurich-raw-to-dslr-tvt train_size=95000 dir_prefix=/kaggle/working/PyNET-v2 model_path=/kaggle/working/PyNET-v2/model.py loss_fn="mse+ssim"
+#cp /kaggle/working/PyNET-v2/models/model.1.best.h5 /kaggle/working/PyNET-v2/models/model.2.9999.h5
